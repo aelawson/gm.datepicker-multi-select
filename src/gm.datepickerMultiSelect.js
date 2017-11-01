edb50977ca32a8c41210e9633387c107ee94a3b2
Andrew Lawson <alawson@jwplayer.com>
2017-11-01 17:02:23 -0400

    Add import and indentation.

--- src/gm.datepickerMultiSelect.js
+++ src/gm.datepickerMultiSelect.js
@@ -21,8 +21,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+import moment from 'moment';
 
-	angular.module('gm.datepickerMultiSelect', ['ui.bootstrap'])
+angular.module('gm.datepickerMultiSelect', ['ui.bootstrap'])
 	.filter('gmISODate', function() {
 	  return function(date) {
 	    return date.toISOString().split("T")[0];
